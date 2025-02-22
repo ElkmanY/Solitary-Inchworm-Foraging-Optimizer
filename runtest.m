e0b8ede70e3f2bceb469e98fa334a82375b83f85
Juchi E. <elkmay@163.com>
2025-02-22 21:14:52 +0800

    Initial commit.

---
+++ runtest.m
@@ -0,0 +1,33 @@
+clearvars
+clc
+
+%%
+
+fcnid = 15;
+obj2 = 'cec17_func';
+
+dim = 30;
+lb = -100*ones(1,dim);
+ub = 100*ones(1,dim);
+iterMax = 1e4;
+runsMax = 1;
+params = [2,1,0.5,100,0];
+
+tic
+for runs = 1:runsMax
+    
+    [f_best(runs,:),x_best(runs,:),f_best_records(:,runs)] ...
+        = SIFO ( obj2,fcnid, dim, lb,ub, iterMax,params);
+
+end
+ct = toc/runsMax;
+
+
+figure(1)
+semilogy(mean(f_best_records,2),'r','LineWidth',2);
+[~,ib] = min(f_best);
+Xbest = x_best(ib,:)
+Best = min(f_best)
+Mean = mean(f_best)
+Worst = max(f_best)
+Std = std(f_best)
